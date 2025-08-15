13c8bc25dc556aba903a43ac1001e7ad900e1271
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-15 02:59:01 +0200

    🎉✨ Add : EventsRoot layout component
    Introduces EventsRootLayout, which includes EventsNavigation and renders nested routes using Outlet. This sets up a layout for event-related pages.

---
+++ frontend/src/pages/EventsRoot.js
@@ -0,0 +1,14 @@
+import EventsNavigation from "../components/EventsNavigation";
+import {Outlet} from "react-router-dom";
+
+function EventsRootLayout(){
+    return (
+        <>
+          <EventsNavigation />
+            <main>
+                <Outlet/>
+            </main>
+        </>
+    )
+}
+export default EventsRootLayout;
