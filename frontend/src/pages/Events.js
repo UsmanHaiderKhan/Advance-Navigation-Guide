88e87d7c7751bdae53caad0ada1cb4e4e345bec3
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-16 17:09:09 +0200

    ♻️ Update : Add loader function to fetch events data
    Introduces an async loader function to fetch events from the backend API and handle errors. This enables data loading for the EventsPage using react-router's data APIs.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,11 +1,21 @@
-
+import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
-import {useLoaderData} from "react-router-dom";
 
 function EventsPage() {
-   const events = useLoaderData();
-
-    return  <EventsList events={events} />
+  const events = useLoaderData();
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
+  } else {
+    const data = await response.json();
+    return data.events;
+  }
+}
