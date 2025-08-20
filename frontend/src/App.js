8039591157f4d4da7e9cdc52a4d06fc1d321a244
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-20 23:48:26 +0200

    🛠️ Refactor :  event form handling and add Home/NewEvent pages
    Unified event form actions by using manipulateEventAction for both new and edit event routes. Updated EditEvent and NewEvent components to specify the HTTP method for EventForm. Added basic Home and NewEvent page implementations.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Home from './pages/Home';
 import EventDetail, {action as eventDeleteAction, loader as eventDetailsLoader} from './pages/Event-Detail';
-import NewEvent, {action as newEventAction} from './pages/NewEvent';
+import NewEvent from './pages/NewEvent';
 import EditEvent from './pages/Edit-Event';
 import Events, {loader} from './pages/Events';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
 import Error from './pages/Error';
+import {action as manipulateEventAction} from './components/EventForm';
 
 const router = createBrowserRouter([
     {
@@ -34,10 +35,10 @@ const router = createBrowserRouter([
                                 action: eventDeleteAction,
                                 element: <EventDetail/>,
                             },
-                            {path: 'edit', element: <EditEvent/>},
+                            {path: 'edit', element: <EditEvent/>, action: manipulateEventAction},
                         ],
                     },
-                    {path: '/events/new', element: <NewEvent/>, action: newEventAction},
+                    {path: '/events/new', element: <NewEvent/>, action: manipulateEventAction},
                 ],
             },
         ],
---
+++ frontend/src/pages/NewEvent.js
@@ -0,0 +1,14 @@
+import EventForm from "../components/EventForm";
+
+function NewEvent() {
+    // function handleSubmit(event) {
+    // event.preventDefault();
+    // }
+  return (
+    <>
+      <EventForm method="post"  />
+    </>
+  );
+}
+export default NewEvent;
+
