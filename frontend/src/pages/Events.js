8facfc60c06a8be85b39213d24bffc762533fd97
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-22 21:12:05 +0200

    ♻️ Update : Add Suspense and Await for async event loading
    Refactored EventsPage to use React Suspense and Await for handling asynchronous event data loading. The loader now returns a promise for events, improving user experience with a loading state.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,35 @@
-import { useLoaderData } from 'react-router-dom';
+import {Suspense} from "react";
+import {Await, useLoaderData} from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
-  return <EventsList events={events} />;
+  const {events} = useLoaderData();
+  return (
+      <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+          <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+          </Await>
+      </Suspense>
+     // <EventsList events={events} />
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
-  if (!response.ok) {
-    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-      status: 500,
-    });
-  } else {
-    const data = await response.json();
-    return data.events;
-  }
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+            status: 500,
+        });
+    } else {
+        const data = await response.json();
+        return data.events;
+    }
 }
+ export function loader() {
+   return {
+       events: loadEvents(),
+   }
+
+ }
